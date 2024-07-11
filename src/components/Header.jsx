37ba460185642363183ac87c6b775e7fc6cb7677
Ch-Sai-Jaswanth <saijaswanthch2003@gmail.com>
2024-07-11 22:06:56 +0530

    Changed Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between items-center bg-gradient-to-r from-blue-500 to-green-500 p-6 md:h-44 shadow-lg p-6 md:h-44 shadow-lg">
+    <div className="flex flex-col md:flex-row justify-between items-center bg-gradient-to-r from-blue-500 to-green-500 p-6 md:h-44 shadow-lg">
       <div className="flex justify-center md:justify-start">
         <img src={LOGO_URL} alt="Weather Logo" className="w-20 md:w-44 m-4 rounded-lg border-2 border-white shadow-md" />
       </div>
