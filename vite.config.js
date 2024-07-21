793143e120739525201cc83aa2acbecbc046b5cc
ChSaiJaswanth <21131A0534@gvpce.ac.in>
2024-07-21 12:51:48 +0530

    Modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     base: './',
     plugins: [react()],
+    optimizeDeps: {
+    include: ['react', 'react-dom', 'react/jsx-runtime']
+    }
 });
